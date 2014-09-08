4ad488a624d85fd19fdbeeda6a856dc63bab3658
babelberry <tatienantes@yahoo.fr>
2014-09-08 17:21:13 +0100

    added new test to add tags

--- lib/server.rb
+++ lib/server.rb
@@ -18,6 +18,13 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
+  post '/links' do
+  	url = params["url"]
+  	title = params["title"]
+  	Link.create(:url => url, :title => title)
+  	redirect to('/')
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -11,11 +11,22 @@ feature "User adds a new link" do
 		expect(link.title).to eq ("Makers Academy")
 	end
 
-	def add_link( url, title )
+	def add_link( url, title, tags=[] )
 		within('#new-link') do
 			fill_in 'url', with: url
 			fill_in 'title', with: title
+			fill_in 'tags', with: tags.join(' ')
 			click_button 'Add link'
 		end
+	end
+
+	scenario "with a few tags" do
+		visit '/'
+		add_link("http://www.makersacademy.com",
+				"Makers Academy",
+				["education", "ruby"])
+		link=Link.first
+		expect(link.tags).to include("education")
+		expect(link.tags).to include("ruby")
 	end
 end
