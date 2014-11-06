40b289b8811890978c5ef79e0066ff6cf3b1afbb
Tatiana Soukiassian <binaryberry@gmail.com>
2014-11-06 10:33:45 +0000

    Fixed 4 tests

--- spec/features/helper/session.rb
+++ spec/features/helper/session.rb
@@ -7,7 +7,7 @@ module SessionHelpers
 				fill_in :email, :with => email
 				fill_in :password, :with => password
 				fill_in :password_confirmation, :with => password_confirmation
-				click_button('Not a collector? Sign up!')
+				click_button "Sign up"
 	end
 
 	def sign_in(email, password)
