6a5c669a79c30555cd8082c150a35d8a6307a9a0
Tatiana Soukiassian <binaryberry@gmail.com>
2014-12-18 09:14:38 +0000

    adding travis CI

--- Gemfile
+++ Gemfile
@@ -10,9 +10,12 @@ gem 'sinatra'
 gem 'shotgun'
 gem 'mailgun'
 gem 'sinatra-partial'
+gem 'rake'
 
 group :development, :test do
 	gem 'capybara'
 	gem "database_cleaner"
   	gem "rspec"
  end
+
+gem "codeclimate-test-reporter", group: :test, require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 # Remember environment variables from week 1?
 $VERBOSE=nil
 ENV["RACK_ENV"] = 'test' # because we need to know what database to work with
-
 # this needs to be after ENV["RACK_ENV"] = 'test'
 # because the server needs to know
 # what environment it's running it: test or development.
